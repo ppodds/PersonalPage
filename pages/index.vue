5daf5c15773bf66a65132c5a3db242082461c969
ppodds <oscar20020629@gmail.com>
2021-09-16 15:42:20 +0800

    Feat: complete Footer

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,17 @@
 <template>
   <main>
     <banner />
+
+    <page-footer />
   </main>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 import Banner from '~/components/index/Banner.vue'
+import PageFooter from '~/components/util/Footer.vue'
 
 export default Vue.extend({
-  components: { Banner },
+  components: { Banner, PageFooter },
 })
 </script>
