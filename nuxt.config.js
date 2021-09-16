d0a48998b472df93008203f5d350dff5413fe4bb
ppodds <oscar20020629@gmail.com>
2021-09-16 21:47:52 +0800

    Feat: edit page title

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'PersonalPage',
+    title: 'ppodds',
     meta: [{ name: 'format-detection', content: 'telephone=no' }],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
